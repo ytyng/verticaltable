47dea312109f14a853d9978db17f938675641a9c
yotsuyanagi <yotsuyanagi@torico-tokyo.com>
2017-12-27 19:39:04 +0900

    upload to pypi.org with twine

--- jjfile.py
+++ jjfile.py
@@ -9,8 +9,6 @@ menu = [
      "./runtests.py"),
     ('flake8',
      "flake8 ."),
-    ('register pypi',
-     "./setup.py register"),
-    ('upload pypi',
-     "./setup.py sdist upload"),
+    ('upload to pypi',
+     "./setup.py sdist; twine upload dist/*"),
 ]
